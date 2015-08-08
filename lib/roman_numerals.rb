0175949b470c811236d78323908aeb16fa8ece25
Tyler Machen <tylermachen@gmail.com>
2015-08-08 16:55:02 -0400

    add to_roman method to Integer class

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+class Integer
+  ROMAN_NUMERALS = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
+
+  def to_roman
+    i = self
+    roman = ''
+    ROMAN_NUMERALS.each do |n, l|
+      roman << l * (i / n)
+      i = i % n
+    end
+    roman
+  end
+end
